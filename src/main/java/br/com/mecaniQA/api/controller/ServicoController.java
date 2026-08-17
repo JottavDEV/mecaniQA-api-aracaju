79f042148edc367b8f1e0658bc29c58dd83f7f7a
JottavDev <jottavdev@icloud.com>
2026-08-16 21:19:09 -0300

    :sparkles: feat: Peca e Servico Controllers
    Enviando Codigo dos Controllers que nivton fez. > definição de rotas

--- src/main/java/br/com/mecaniQA/api/controller/ServicoController.java
+++ src/main/java/br/com/mecaniQA/api/controller/ServicoController.java
@@ -1,5 +1,70 @@
 package br.com.mecaniQA.api.controller;
 
+import br.com.mecaniQA.api.model.Servico;
+import br.com.mecaniQA.api.repository.ServicoRepository;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/servicos")
 public class ServicoController {
 
+    // ===================== REPOSITORY =====================
+
+    private final ServicoRepository repository;
+
+    // ===================== CONSTRUTOR =====================
+
+    public ServicoController() {
+        this.repository = ServicoRepository.getInstance();
+    }
+
+    // ===================== CREATE =====================
+
+    @PostMapping
+    public Servico criar(@RequestBody Servico servico) {
+
+        return repository.salvar(servico);
+    }
+
+    // ===================== READ - TODOS =====================
+
+    @GetMapping
+    public List<Servico> listarTodos() {
+
+        return repository.getServicos();
+    }
+
+    // ===================== READ - POR ID =====================
+
+    @GetMapping("/{codigoServico}")
+    public Servico buscarPorId(
+            @PathVariable long codigoServico) {
+
+        return repository.buscarPorID(codigoServico);
+    }
+
+    // ===================== UPDATE =====================
+
+    @PutMapping("/{codigoServico}")
+    public Servico atualizar(
+            @PathVariable long codigoServico,
+            @RequestBody Servico servico) {
+
+        return repository.atualizar(
+                codigoServico,
+                servico
+        );
+    }
+
+    // ===================== DELETE =====================
+
+    @DeleteMapping("/{codigoServico}")
+    public boolean deletar(
+            @PathVariable long codigoServico) {
+
+        return repository.deletar(codigoServico);
+    }
 }
